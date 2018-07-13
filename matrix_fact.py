1fe4dd19121a8149e7636042e287095d685f61de
neiu11 <atchusl@yahoo.es>
2018-07-13 15:00:17 +0200

    Add files via upload

---
+++ matrix_fact.py
@@ -0,0 +1,48 @@
+# Created by Albert Au Yeung (2010)
+#
+# An implementation of matrix factorization
+#
+try:
+    import numpy as np
+except:
+    print ("This implementation requires the np module.")
+    exit(0)
+
+from progressbar import ProgressBar
+pbar = ProgressBar()
+###############################################################################
+
+"""
+@INPUT:
+    R     : a matrix to be factorized, dimension N x M
+    P     : an initial matrix of dimension N x K
+    Q     : an initial matrix of dimension M x K
+    K     : the number of latent features
+    steps : the maximum number of steps to perform the optimisation
+    alpha : the learning rate
+    beta  : the regularization parameter
+@OUTPUT:
+    the final matrices P and Q
+"""
+def matrix_factorization(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
+    Q = Q.T
+    for step in pbar(range(steps)):
+        print(step);
+        for i in (range(len(R))):
+            for j in range(len(R[i])):
+                if R[i][j] > 0:
+                    eij = R[i][j] - np.dot(P[i,:],Q[:,j])
+                    for k in (range(K)):
+                        P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
+                        Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
+        eR = np.dot(P,Q)
+        e = 0
+        for i in (range(len(R))):
+            for j in (range(len(R[i]))):
+                if R[i][j] > 0:
+                    e = e + pow(R[i][j] - np.dot(P[i,:],Q[:,j]), 2)
+                    for k in (range(K)):
+                        e = e + (beta/2) * ( pow(P[i][k],2) + pow(Q[k][j],2) )
+        if e < 0.001:
+            break
+    return P, Q.T
